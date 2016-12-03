2c34bae18d799034d7eb58ad28f11d06ca01cc84
Kent <kent.sinclair@gmail.com>
2016-12-03 17:34:54 -0500

    login working

--- ClientApplication/renderer-process/chat.js
+++ ClientApplication/renderer-process/chat.js
@@ -1,18 +1,33 @@
+// pass login request to Server -- returns ID used to login
+var user;
+var ws;
+
+$(function(){
+  $('#login').submit(function(e){
+    e.preventDefault();
+
+    var form = $(this);
+    var username = $("input[name=username]").val();
+    console.log(username);
+    login(username);
+  });
+})
 
 
-
-
-
-// todo need to implement username and server info
-var ws = new WebSocket('ws://hardcore.com:8080', 'echo-protocol');
-
-function sendMessage(){
-    var message = document.getElementById('message').value;
-    ws.send(message);
-}
-
-ws.addEventListener("message", function(e){
-    var msg = e.data;
-
-    document.getElementById('chatlog').innerHTML += '<br>' + msg;
+function login(userName){
+console.log(userName);  
+  $.ajax({
+    type: 'POST',
+    url: 'http://localhost:3000/login',
+    contentType: 'application/json',
+    data: JSON.stringify({name: userName}),
+    success: function(result){
+      console.log(result)
+      user = JSON.parse(result)
+      ws = new WebSocket('ws://localhost:8080/'+user._id);
+    },
+    error: function(result){
+      console.log(result.responseText)
+    }
 });
+}
